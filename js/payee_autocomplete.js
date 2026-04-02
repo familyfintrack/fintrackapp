b8cd8e2e2b3956b90e8e90e72b2a3378572a05db
familyfintrack <269470868+familyfintrack@users.noreply.github.com>
2026-04-01 23:43:46 -0300

    Delete js directory

--- js/payee_autocomplete.js
+++
@@ -1,9 +0,0 @@
-// Legacy shim: payee autocomplete now lives in utils.js
-// Keep this file loaded for backward compatibility without redeclaring globals.
-(function(){
-  try {
-    if (typeof window !== 'undefined') {
-      window.__payeeAutocompleteShimLoaded = true;
-    }
-  } catch (_) {}
-})();
